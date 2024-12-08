f15c8a17eba436f062621b63bcd62e4b7ef46725
Dusk <55848332+Zv-10@users.noreply.github.com>
2024-12-08 18:50:45 +0700

    beta

---
+++ vet mangement system4-20241014T045917Z-006/test beta v2/vet mangement system4-/vet mangement system4-20241014T045917Z-001/vet mangement system/client_info.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class client_info
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(client_info))
+        Me.Txt_fname = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txt_lname = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Txt_contactnum = New System.Windows.Forms.TextBox
+        Me.Txt_address = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Txt_fullname = New System.Windows.Forms.TextBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.btn_add = New System.Windows.Forms.Button
+        Me.txt_cid = New System.Windows.Forms.TextBox
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton
+        Me.GroupBox1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Txt_fname
+        '
+        Me.Txt_fname.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_fname.ForeColor = System.Drawing.Color.Black
+        Me.Txt_fname.Location = New System.Drawing.Point(93, 190)
+        Me.Txt_fname.Name = "Txt_fname"
+        Me.Txt_fname.Size = New System.Drawing.Size(133, 27)
+        Me.Txt_fname.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(92, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 29)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "FIRST NAME"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(261, 158)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 29)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "LAST NAME"
+        '
+        'txt_lname
+        '
+        Me.txt_lname.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_lname.ForeColor = System.Drawing.Color.Black
+        Me.txt_lname.Location = New System.Drawing.Point(264, 190)
+        Me.txt_lname.Name = "txt_lname"
+        Me.txt_lname.Size = New System.Drawing.Size(133, 27)
+        Me.txt_lname.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(88, 297)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(197, 29)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "CONTACT NUMBER"
+        '
+        'Txt_contactnum
+        '
+        Me.Txt_contactnum.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_contactnum.ForeColor = System.Drawing.Color.Black
+        Me.Txt_contactnum.Location = New System.Drawing.Point(93, 329)
+        Me.Txt_contactnum.Name = "Txt_contactnum"
+        Me.Txt_contactnum.Size = New System.Drawing.Size(246, 27)
+        Me.Txt_contactnum.TabIndex = 7
+        Me.Txt_contactnum.Text = "09"
+        '
+        'Txt_address
+        '
+        Me.Txt_address.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_address.ForeColor = System.Drawing.Color.Black
+        Me.Txt_address.Location = New System.Drawing.Point(89, 410)
+        Me.Txt_address.Name = "Txt_address"
+        Me.Txt_address.Size = New System.Drawing.Size(822, 27)
+        Me.Txt_address.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(93, 365)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 29)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "ADDRESS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(94, 229)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(123, 29)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "FULL NAME"
+        '
+        'Txt_fullname
+        '
+        Me.Txt_fullname.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt_fullname.ForeColor = System.Drawing.Color.Black
+        Me.Txt_fullname.Location = New System.Drawing.Point(89, 265)
+        Me.Txt_fullname.Name = "Txt_fullname"
+        Me.Txt_fullname.Size = New System.Drawing.Size(250, 27)
+        Me.Txt_fullname.TabIndex = 11
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btn_add)
+        Me.GroupBox1.Controls.Add(Me.txt_cid)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Txt_fname)
+        Me.GroupBox1.Controls.Add(Me.txt_lname)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Txt_fullname)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Txt_contactnum)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Txt_address)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 46)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1300, 655)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "COSTOMER DETAILS"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.AliceBlue
+        Me.Button1.Location = New System.Drawing.Point(531, 537)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 43)
+        Me.Button1.TabIndex = 23
+        Me.Button1.Text = "VIEW DATA"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'btn_add
+        '
+        Me.btn_add.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btn_add.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_add.ForeColor = System.Drawing.Color.SeaShell
+        Me.btn_add.Location = New System.Drawing.Point(248, 537)
+        Me.btn_add.Name = "btn_add"
+        Me.btn_add.Size = New System.Drawing.Size(177, 43)
+        Me.btn_add.TabIndex = 0
+        Me.btn_add.Text = "ADD DATA"
+        Me.btn_add.UseVisualStyleBackColor = False
+        '
+        'txt_cid
+        '
+        Me.txt_cid.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_cid.ForeColor = System.Drawing.Color.Black
+        Me.txt_cid.Location = New System.Drawing.Point(95, 123)
+        Me.txt_cid.Name = "txt_cid"
+        Me.txt_cid.Size = New System.Drawing.Size(147, 27)
+        Me.txt_cid.TabIndex = 15
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(93, 88)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(149, 29)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "COSTOMER ID"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ToolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripButton2, Me.ToolStripSeparator1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1300, 25)
+        Me.ToolStrip1.TabIndex = 24
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.BackColor = System.Drawing.Color.Black
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton2.Image = Global.vet_mangement_system.My.Resources.Resources.fed81b8b054414eb4e0d859d6cbd50ce_removebg_preview
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton2.Text = "ToolStripButton2"
+        '
+        'client_info
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(1300, 739)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.HelpButton = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "client_info"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.TopMost = True
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Txt_fname As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txt_lname As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Txt_contactnum As System.Windows.Forms.TextBox
+    Friend WithEvents Txt_address As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Txt_fullname As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txt_cid As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents btn_add As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStripButton1 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripButton2 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+End Class
---
+++ vet mangement system4-20241014T045917Z-006/test beta v2/vet mangement system4-/vet mangement system4-20241014T045917Z-001/vet mangement system/pet_view.vb
@@ -0,0 +1,256 @@
+﻿
+Imports System.Windows.Forms
+Imports System.Data.Odbc
+
+
+Public Class pet_view
+    Dim gender As String
+   
+    
+
+   
+Private Sub pet_view_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        refreshMe()
+End Sub
+
+    Private Sub btn_edit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_edit.Click
+        'edit enable  txtbox
+
+        cmb_breed.Enabled = True
+        txt_color.Enabled = True
+        txt_petname.Enabled = True
+        cmb_species.Enabled = True
+
+        DateTimePicker1.Enabled = True
+        FemaleRadio.Enabled = True
+        MaleRadio.Enabled = True
+
+
+        txt_petname.Focus()
+
+        'edit enable buttons
+        btn_edit.Enabled = False
+        btn_delete.Enabled = False
+        btn_update.Enabled = True
+
+
+
+
+    End Sub
+
+    Private Sub refreshMe()
+        con.Close()
+
+
+        Try
+            con.Open()
+
+            Dim myCmd1 As New OdbcCommand("Select * from vet_info", con)
+            Dim da As New OdbcDataAdapter(myCmd1)
+            Dim ds As New System.Data.DataSet
+
+            da.Fill(ds, "vet_info")
+
+            dgv_petview.DataSource = ds.Tables(0)
+            dgv_petview.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+
+            dgv_petview.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            dgv_petview.Refresh()
+            ' Set the default font style and color for the DataGridView
+            dgv_petview.DefaultCellStyle.Font = New Font("Verdana", 8, FontStyle.Regular)
+            dgv_petview.DefaultCellStyle.ForeColor = Color.Black
+            dgv_petview.DefaultCellStyle.BackColor = Color.Gray
+
+            ' Set alternate row color for better readability
+            Dim alternatingStyle As New DataGridViewCellStyle()
+            alternatingStyle.BackColor = Color.WhiteSmoke
+            dgv_petview.AlternatingRowsDefaultCellStyle = alternatingStyle
+
+            ' Set header style
+            Dim headerStyle As New DataGridViewCellStyle()
+            headerStyle.Font = New Font("Verdana", 8, FontStyle.Bold)
+            headerStyle.ForeColor = Color.White
+            headerStyle.BackColor = Color.Black
+
+            ' Apply header style to the DataGridView
+            dgv_petview.ColumnHeadersDefaultCellStyle = headerStyle
+            dgv_petview.Refresh()
+            dgv_petview.Refresh()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        Finally
+
+
+            con.Close()
+        End Try
+
+    End Sub
+
+    Private Sub btn_update_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_update.Click
+        Dim vid As String
+
+        Dim c As String
+        Dim s As String
+        Dim p As String
+        Dim br As String
+        Dim bd As String
+        Dim f As String
+       
+        'dim cl as integer'
+
+        gender = ""
+
+
+
+        vid = txt_vid.Text
+
+        c = txt_color.Text.ToUpper.Trim
+        s = cmb_species.Text.Trim
+        p = txt_petname.Text.Trim
+        br = cmb_breed.Text.Trim
+
+        If FemaleRadio.Checked Then
+            gender = "FEMALE"
+        ElseIf MaleRadio.Checked Then
+            gender = "MALE"
+        End If
+
+
+
+        bd = DateTimePicker1.Text.Trim
+        f = Txt_fullname.Text.Trim
+
+
+        'c = txt_copies.text.trim'
+        ' cl = CInt(c)'
+        con.Close()
+
+        Try
+            con.Open()
+            With mycmd
+                .Connection = con
+                .CommandText = "update  vet_info set pet_id= '" & txt_vid.Text & "',pet_name= '" & txt_petname.Text.ToUpper.ToUpper & "' ,pet_color= '" & txt_color.Text & "' ,animal_species ='" & cmb_species.Text & "', pet_breed= '" & cmb_breed.Text & "', pet_gender= '" & gender & "', pet_birthday=  '" & bd & "',fullname= '" & Txt_fullname.Text.ToUpper.Trim & "' ,lname= '" & txt_lname.Text.ToUpper.Trim & "' ,fname= '" & txt_fname.Text.ToUpper.Trim & "'  where pet_id= '" & vid & "'"
+                .ExecuteNonQuery()
+                .ExecuteNonQuery()
+            End With
+            MessageBox.Show("information has been updated", " update information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+
+
+            refreshMe()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            con.Close()
+
+        End Try
+    End Sub
+
+    Private Sub ToolStripButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub dgv_petview_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgv_petview.CellContentClick
+
+
+
+        txt_vid.Text = dgv_petview.Item("pet_id", dgv_petview.CurrentRow.Index).Value
+        txt_petname.Text = dgv_petview.Item("pet_name", dgv_petview.CurrentRow.Index).Value
+
+        txt_color.Text = dgv_petview.Item("pet_color", dgv_petview.CurrentRow.Index).Value
+        cmb_species.Text = dgv_petview.Item("animal_species", dgv_petview.CurrentRow.Index).Value
+        cmb_breed.Text = dgv_petview.Item("pet_breed", dgv_petview.CurrentRow.Index).Value
+
+        If dgv_petview.Item("pet_gender", dgv_petview.CurrentRow.Index).Value = "MALE" Then
+            MaleRadio.Checked = True
+            FemaleRadio.Checked = False
+        ElseIf dgv_petview.Item("pet_gender", dgv_petview.CurrentRow.Index).Value = "FEMALE" Then
+            MaleRadio.Checked = False
+            FemaleRadio.Checked = True
+        End If
+
+        DateTimePicker1.Value = dgv_petview.Item("pet_birthday", dgv_petview.CurrentRow.Index).Value
+
+        Txt_fullname.Text = dgv_petview.Item("fullname", dgv_petview.CurrentRow.Index).Value
+        txt_fname.Text = dgv_petview.Item("fname", dgv_petview.CurrentRow.Index).Value
+        txt_lname.Text = dgv_petview.Item("lname", dgv_petview.CurrentRow.Index).Value
+
+
+
+
+
+
+        'enable txtbox
+        txt_vid.Enabled = False
+        txt_petname.Enabled = False
+
+        txt_color.Enabled = False
+        cmb_species.Enabled = False
+        cmb_species.Enabled = False
+        DateTimePicker1.Enabled = False
+        DateTimePicker1.Enabled = False
+        Txt_fullname.Enabled = False
+        MaleRadio.Enabled = False
+        FemaleRadio.Enabled = False
+
+
+
+        'enabled button 
+
+        btn_edit.Enabled = True
+        btn_delete.Enabled = True
+        btn_update.Enabled = False
+    End Sub
+
+    Private Sub btn_delete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_delete.Click
+        Dim result2 As New DialogResult
+        Dim bid1 As String
+
+        bid1 = txt_vid.Text
+
+        connectMe()
+
+        result2 = MessageBox.Show(" are you sure you want to delete the record?", "confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result2 = Windows.Forms.DialogResult.Yes Then
+            With mycmd
+                .Connection = con
+                .CommandText = "delete from vet_info where pet_id = '" & bid1 & "'"
+                .ExecuteNonQuery()
+            End With
+            MessageBox.Show(" record deleted", "delete recods", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FemaleRadio.CheckedChanged
+        gender = "female"
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MaleRadio.CheckedChanged
+        gender = "male"
+    End Sub
+
+    Private Sub ToolStripButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txt_fname_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txt_fname.KeyDown
+        Txt_fullname.Text = txt_fname.Text.ToUpper.Trim & "" & txt_lname.Text.ToUpper.Trim
+
+
+    End Sub
+
+  
+
+    Private Sub txt_lname_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt_lname.TextChanged
+        Txt_fullname.Text = txt_fname.Text.ToUpper.Trim & "" & txt_lname.Text.ToUpper.Trim
+    End Sub
+
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
